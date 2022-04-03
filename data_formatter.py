6a6360fdc2cbafa49ae4067019e9cf5d4504e18f
Nathan Merz <nmerz@icloud.com>
2022-04-02 23:16:18 -0400

    Add conversion of ids to ints
    https://github.com/pandeyshubham25/pagerank/issues/5#issuecomment-1086760548

--- data_formatter.py
+++ data_formatter.py
@@ -6,11 +6,15 @@ out_folder.mkdir(parents=True, exist_ok=True)
 NODE_THRESHOLD = 183964077 / 3 #File is about 2x too large, so cut it down with some safety margin
 
 for raw_filepath in Path("data_raw").iterdir():
+    next_node = 0
+    node_map = dict()
     with open(raw_filepath) as raw_file:
+        print(f"Processing {raw_filepath}")
         with open(out_folder / raw_filepath.stem, "w") as out_file:
             start_copy = False;
             skip_next = False;
             for raw_line in raw_file:
+                raw_line = raw_line.strip()
                 if (start_copy):
                     if (skip_next):
                         skip_next = False
@@ -23,11 +27,17 @@ for raw_filepath in Path("data_raw").iterdir():
                             continue
                         elif (node2 > NODE_THRESHOLD):
                             break
-                    if raw_filepath.name == "rec-amz-Books.edges":
+                    elif raw_filepath.name == "rec-amz-Books.edges":
                         node1, node2, weights, timestamp = raw_line.split(",")
-                        out_file.write(node1 + " " + node2 + "\n")
                     else:
-                        out_file.write(raw_line)
+                        node1, node2 = raw_line.split()
+                    if (node1 not in node_map.keys()):
+                        node_map[node1] = next_node
+                        next_node += 1
+                    if (node2 not in node_map.keys()):
+                        node_map[node2] = next_node
+                        next_node += 1
+                    out_file.write(str(node_map[node1]) + " " + str(node_map[node2]) + "\n")
                 elif not raw_line.startswith("%") and not raw_line.startswith("#"):
                     start_copy = True
                     if raw_filepath.suffix == ".mtx":
