4622435c6b552b7678fd128de60f089ea400b7c2
Nathan Merz <NMerz@users.noreply.github.com>
2022-04-11 16:53:00 -0400

    Generalize prank run for source of truth
    Prepare way for separate source of truth handling

--- src/measure.cpp
+++ src/measure.cpp
@@ -15,11 +15,30 @@
 #define ALPHA 0.85
 
 
+void run_pr_iteration(std::string pr_version, std::ofstream &gp_data, std::string filename, bool first) {
+            char pr_command[300];
+            std::snprintf(pr_command, 300, "./%s %d, %d, %f, data/%s\n", pr_version.c_str(), PROCESSORS, NUM_ITERS, ALPHA, filename.c_str());
+            std::cout << pr_command;
+            FILE *pr_output = popen(pr_command, "r");
+            char time_str[50];
+            fgets(time_str, 50, pr_output);
+            double runtime = atof(time_str);
+            if (first) {
+                gp_data << filename << ",";
+            } else {
+                gp_data << ",";
+            }
+            gp_data << runtime;
+            pclose(pr_output);
+}
+
 int main() {
 #ifdef TEST
-    std::vector<std::string> PR_VERSIONS = {"test/pr_mock_long", "test/pr_mock", "test/pr_mock_short"};
+    std::vector<std::string> PR_VERSIONS = {"test/pr_mock_long", "test/pr_mock_short"};
+    std::string pr_source_of_truth = "test/pr_mock";
 #else
-    std::vector<std::string> PR_VERSIONS = {"pagerank"};
+    std::vector<std::string> PR_VERSIONS = {"prank_parallel", "prank_trivial_parallel"};
+    std::string pr_source_of_truth = "prank_serial";
 #endif
     namespace fs = std::filesystem;
     std::ofstream gp_data("gnuplot_data", std::ofstream::out );
@@ -34,25 +53,10 @@ int main() {
         gp_data << pr_version;
     }
     gp_data << "\n";
-        for (fs::directory_entry data_file : fs::directory_iterator("data")) {
-        std::vector<double> runtimes;
-        first = true;
-    for (std::string pr_version : PR_VERSIONS) {
-            char pr_command[300];
-            std::snprintf(pr_command, 300, "./%s %d, %d, %f, data/%s\n", pr_version.c_str(), PROCESSORS, NUM_ITERS, ALPHA, data_file.path().filename().string().c_str());
-            std::cout << pr_command;
-            FILE *pr_output = popen(pr_command, "r");
-            char time_str[50];
-            fgets(time_str, 50, pr_output);
-            double runtime = atof(time_str);
-            if (first) {
-                first = false;
-                gp_data << data_file.path().filename().string() << ",";
-            } else {
-                gp_data << ",";
-            }
-            gp_data << runtime;
-            pclose(pr_output);
+    for (fs::directory_entry data_file : fs::directory_iterator("data")) {
+        run_pr_iteration(pr_source_of_truth, gp_data, data_file.path().filename().string(), true);
+        for (std::string pr_version : PR_VERSIONS) {
+            run_pr_iteration(pr_version, gp_data, data_file.path().filename().string(), false);
         }
         gp_data << "\n";
     }
@@ -60,7 +64,7 @@ int main() {
     gp_instructions << "set key autotitle columnhead noenhanced\n";
     gp_instructions << "set datafile separator \",\"\n";
     char plot_command[500];
-    std::snprintf(plot_command, 500, "plot for [i=2:%lu] \"gnuplot_data\"  using (column(0)):i:xtic(1)\n", PR_VERSIONS.size() + 1);
+    std::snprintf(plot_command, 500, "plot for [i=2:%lu] \"gnuplot_data\"  using (column(0)):i:xtic(1)\n", PR_VERSIONS.size() + 2);
     gp_instructions << plot_command;
     gp_data.close();
     gp_instructions.close();
